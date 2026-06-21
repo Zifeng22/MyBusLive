413740a703bff0d96b1c49cf62a31b656d54df8b
sovaleow <leowweiru@gmail.com>
2026-06-21 19:15:39 +0800

    Add BookingController and BusController for managing bookings and buses

--- bus-system/src/main/java/com/transitmy/bus_system/Repository/BookingRepository.java
+++ bus-system/src/main/java/com/transitmy/bus_system/Repository/BookingRepository.java
@@ -1,9 +1,12 @@
 package com.transitmy.bus_system.Repository;
 
-import org.springframework. data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.transitmy.bus_system.entity.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    Booking findByBookingReference(String bookingReference);
+    List<Booking> findByUserUserID(Long userID);
+    List<Booking> findByBusBusID(Long busID);
 }
