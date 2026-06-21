0c003bdc663b7ae1f503af1da35c7df9fafa9185
Zifeng22 <linmonzfon@gmail.com>
2026-06-21 21:57:50 +0800

    Refactor: Update package names to lowercase for consistency and adjust import statements in controllers

--- bus-system/src/main/java/com/transitmy/bus_system/controller/BookingController.java
+++ bus-system/src/main/java/com/transitmy/bus_system/controller/BookingController.java
@@ -4,12 +4,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.transitmy.bus_system.Repository.BookingRepository;
-import com.transitmy.bus_system.Repository.BusRepository;
 import com.transitmy.bus_system.entity.Booking;
 import com.transitmy.bus_system.entity.Bus;
+import com.transitmy.bus_system.repository.BookingRepository;
+import com.transitmy.bus_system.repository.BusRepository;
 
 @RestController
 @RequestMapping("/api/bookings")
--- bus-system/src/main/java/com/transitmy/bus_system/controller/BusController.java
+++ bus-system/src/main/java/com/transitmy/bus_system/controller/BusController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.transitmy.bus_system.Repository.BusRepository;
+import com.transitmy.bus_system.repository.BusRepository;
 import com.transitmy.bus_system.entity.Bus;
 
 @RestController
