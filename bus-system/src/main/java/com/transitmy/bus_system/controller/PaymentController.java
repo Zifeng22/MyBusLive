4316b23e32a238c32e51e4207b6df303064d6dba
Zifeng22 <linmonzfon@gmail.com>
2026-07-02 00:10:44 +0800

    Add PUT endpoint to cancel payment in PaymentController

--- bus-system/src/main/java/com/transitmy/bus_system/controller/PaymentController.java
+++ bus-system/src/main/java/com/transitmy/bus_system/controller/PaymentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,6 +63,9 @@ public class PaymentController {
     public Payment getPaymentById(@PathVariable Long paymentID) {
         return paymentRepository.findById(paymentID).orElseThrow();
     }
+
+    @PutMapping("/payments/{paymentID}/cancel")
+    
 
     
     
